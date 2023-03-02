a90bd30ede651ac6ff6117fc568073a2b46bf4c1
JoaoPedro <cpsenha@gmail.com>
2023-03-02 18:49:16 -0300

    Versão não finalizada

---
+++ src/components/Lista/index.tsx
@@ -0,0 +1,21 @@
+import Item from './Item';
+import style from './Lista.module.scss';
+import { ITarefa } from '../../types/tarefa';
+
+function Lista({ tarefas }:{ tarefas:  ITarefa[]}) {
+    return (
+        <aside className={style.listaTarefas}>
+            <h2>Estudos do Dia</h2>
+            <ul>
+                {tarefas.map((item, index) => (
+                    <Item
+                        key={index} 
+                        {...item}
+                   />
+                ))}
+            </ul>
+        </aside>
+    )
+}
+
+export default Lista;
